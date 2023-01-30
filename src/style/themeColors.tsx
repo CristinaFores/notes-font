4b443c6ebcc7bac357b37780ed85b276268b7b9c
Cristina <cristinaforescampos1992@gmail.com>
2023-01-30 22:13:56 +0100

    Customize mainStyleColors, add colors button

---
+++ src/style/themeColors.tsx
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const mainStyleColors: DefaultTheme = {
+  colors: {
+    primary: {
+      base: "#666666",
+      dark: "#000",
+      light: "#fff",
+    },
+    button: {
+      active: "#b388ff",
+      hover: "#711fb0",
+      disable: "rgba(113, 31, 176, 0.501)",
+    },
+  },
+};
+
+export default mainStyleColors;
