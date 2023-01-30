9e166fbd6890e79bcb00231585ffd2e74c6d02bc
Cristina <cristinaforescampos1992@gmail.com>
2023-01-30 21:51:12 +0100

    Create components Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { HeaderStyled } from "./HeaderStyled";
+
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <HeaderStyled>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <h1>Blackboards & Notes</h1>
+      </HeaderStyled>
+    </>
+  );
+};
+
+export default Header;
