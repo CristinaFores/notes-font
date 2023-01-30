7fbcfff4c56b6db365b4e9ad03b7650f27fff576
Cristina <cristinaforescampos1992@gmail.com>
2023-01-30 22:31:40 +0100

    Customize Input styles

---
+++ src/components/Input/InputStyled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const InputStyled = styled.input`
+  border: none;
+  border-bottom: solid 2px ${(props) => props.theme.colors.primary.dark};
+  width: 100%;
+  margin-bottom: 1.8rem;
+  margin-top: 1.25px;
+  display: block;
+  border-radius: 6px;
+  padding: 0.3rem 3px;
+
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid ${(props) => props.theme.colors.primary.dark};
+  }
+`;
+
+export const InputLabelStyled = styled.label`
+  font-weight: bold;
+`;
