33264719c95ddb85c18907c7873c99458b1f6469
Cristina <cristinaforescampos1992@gmail.com>
2023-01-30 21:49:08 +0100

    Add in index:  GlobalStyle  and BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./components/App/App";
+import reportWebVitals from "./reportWebVitals";
+import mainStyleColors from "./style/themeColors";
+import "@fontsource/montserrat";
+import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={mainStyleColors}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
