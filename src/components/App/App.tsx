11c2b1a7bb648d1dd9aec839303491219d775b03
Cristina <cristinaforescampos1992@gmail.com>
2023-01-31 17:35:31 +0100

    Add routers in component App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import Header from "../Header/Header";
+import Login from "../Login/Login";
 import Register from "../Register/Register";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <Header />
-      <Register />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 };
