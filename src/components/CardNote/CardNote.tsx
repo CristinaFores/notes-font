85969da07424be20019b92578adf43302af56631
Cristina <cristinaforescampos1992@gmail.com>
2023-01-31 20:13:01 +0100

    Create component CardNotes

---
+++ src/components/CardNote/CardNote.tsx
@@ -0,0 +1,33 @@
+import { CardNoteStyled, ContainIconEditStyled } from "./CardNoteStyled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenToSquare,
+  faCircleXmark,
+} from "@fortawesome/free-regular-svg-icons";
+
+interface CardNoteProps {
+  title: string;
+  date?: string;
+  description?: string;
+}
+const CardNote = ({ title, date, description }: CardNoteProps): JSX.Element => {
+  return (
+    <>
+      <CardNoteStyled>
+        <ContainIconEditStyled>
+          <button onClick={() => {}} aria-label="editar">
+            <FontAwesomeIcon className="icon-edit" icon={faPenToSquare} />
+          </button>
+          <button onClick={() => {}} aria-label="borrar">
+            <FontAwesomeIcon className="icon-edit" icon={faCircleXmark} />
+          </button>
+        </ContainIconEditStyled>
+        <h4>{title}</h4>
+        <span>{date}</span>
+        <p>{description}</p>
+      </CardNoteStyled>
+    </>
+  );
+};
+
+export default CardNote;
