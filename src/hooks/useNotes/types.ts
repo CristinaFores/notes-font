2e6daa901796d1105354ebe4ded6e80fca8072c9
Cristina <cristinaforescampos1992@gmail.com>
2023-01-31 23:07:27 +0100

    Add types Note and UiState

---
+++ src/hooks/useNotes/types.ts
@@ -0,0 +1,20 @@
+export interface Note {
+  id?: string;
+  owner?: {
+    username: string;
+    id?: string;
+  };
+  title: string;
+  description: string;
+  date?: string;
+  buckpicture?: string[];
+}
+
+export interface UiState {
+  modal: {
+    text: string;
+    showModal: boolean;
+    isError: boolean;
+  };
+  isLoading: boolean;
+}
