ac75b19958e8c581eddbda1c6f3692949d33b416
Cristina <cristinaforescampos1992@gmail.com>
2023-01-31 17:48:42 +0100

    Test component Login

---
+++ src/components/Login/Login.test.tsx
@@ -0,0 +1,49 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "../../hooks/useUser/useUser";
+import useUser from "../../hooks/useUser/useUser";
+import { ProviderWrapper } from "../../utilsTest/utilsTest";
+import Login from "./Login";
+
+jest.mock("../../hooks/useUser/useUser", () => {
+  const login = jest.fn();
+  return () => ({ login });
+});
+
+describe("Given Login component", () => {
+  describe("When it us rendered", () => {
+    test("Then its should show input with text: 'Nombre', a button with text 'Entrar'and  link with text: 'Registrarte'", async () => {
+      const { login } = useUser();
+
+      const nameLink = "Registrarte";
+      const nameButton = "Entrar";
+
+      render(<ProviderWrapper children={<Login />} />);
+
+      const renderLink = screen.getByRole("link", {
+        name: nameLink,
+      });
+      const expectIinputName = screen.queryByRole("textbox", {
+        name: "Nombre",
+      })!;
+      const expectIinputEmail = screen.queryByRole("textbox", {
+        name: "Email",
+      })!;
+      const passwordInput = screen.queryByLabelText("Contraseña")!;
+      const button = screen.queryByRole("button", {
+        name: nameButton,
+      })!;
+
+      await userEvent.type(expectIinputName, "1234567891");
+      await userEvent.type(expectIinputEmail, "cris@mil.com");
+      await userEvent.type(passwordInput, "Cristina");
+      await userEvent.click(button);
+
+      expect(login).toHaveBeenCalled();
+      expect(expectIinputName).toBeInTheDocument();
+      expect(expectIinputEmail).toBeInTheDocument();
+      expect(passwordInput).toBeInTheDocument();
+      expect(renderLink).toBeInTheDocument();
+    });
+  });
+});
