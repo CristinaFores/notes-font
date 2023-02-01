9b17918fb709b93899937ff3c391b04f5e17c7f9
Cristina <cristinaforescampos1992@gmail.com>
2023-02-01 19:06:05 +0100

    Create Layout and  add routers, header and navbar

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import { LayoutStyled } from "./LayoutStyled";
+import { Route, Routes } from "react-router";
+import Header from "../Header/Header";
+import ListNotes from "../ListNotes/ListNotes";
+import Login from "../Login/Login";
+import Navbar from "../NavBar/Navbar";
+import Register from "../Register/Register";
+
+const Layout = (): JSX.Element => {
+  const username = localStorage.getItem("username");
+
+  return (
+    <>
+      <Header />
+      {username !== "" ? (
+        <Navbar
+          name={
+            username?.toUpperCase().substring(0, 1)! + username?.substring(1)
+          }
+        />
+      ) : null}
+
+      <LayoutStyled>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<ListNotes />} />
+        </Routes>
+      </LayoutStyled>
+    </>
+  );
+};
+
+export default Layout;
