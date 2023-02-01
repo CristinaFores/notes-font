198b15d272d3bcf3018510d6396374f80f0d6925
Cristina <cristinaforescampos1992@gmail.com>
2023-02-01 19:07:31 +0100

    Delete routers in component App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,9 @@
-import React from "react";
-import { Route, Routes } from "react-router";
-import Header from "../Header/Header";
-import ListNotes from "../ListNotes/ListNotes";
-import Login from "../Login/Login";
-import Register from "../Register/Register";
+import Layout from "../Layout/Layout";
 
 const App = (): JSX.Element => {
   return (
     <div>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<ListNotes />} />
-      </Routes>
+      <Layout />
     </div>
   );
 };
