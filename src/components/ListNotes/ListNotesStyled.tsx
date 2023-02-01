a65d7a4d9fef2c3db9eb9dbb6a101a17d9e0b404
Cristina <cristinaforescampos1992@gmail.com>
2023-02-01 19:04:27 +0100

    Change styles ListNotes, gap

--- src/components/ListNotes/ListNotesStyled.tsx
+++ src/components/ListNotes/ListNotesStyled.tsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const ListNotesStyled = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-  gap: 1rem;
+  gap: 1.2rem;
 `;
