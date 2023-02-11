2cd5e8b2225c5670b8ab7ec5585dca3b1cd6a4e1
Cristina <cristinaforescampos1992@gmail.com>
2023-02-11 23:41:14 +0100

    Fix styles grid, component ListNotes

--- src/components/ListNotes/ListNotesStyled.tsx
+++ src/components/ListNotes/ListNotesStyled.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const ListNotesStyled = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
   gap: 1.2rem;
 `;
