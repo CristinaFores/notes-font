e2677bf7e071e3d0d8729019082e63f473949843
Cristina <cristinaforescampos1992@gmail.com>
2023-02-11 23:16:59 +0100

    Create data,Trello

---
+++ src/data/dataTrello.tsx
@@ -0,0 +1,26 @@
+import { Data } from "../components/Trello/Types";
+
+export const data: Data = {
+  columns: [
+    {
+      id: "column-1",
+      title: "To do 📑",
+      items: [],
+    },
+    {
+      id: "column-2",
+      title: "In Progress💻",
+      items: [],
+    },
+    {
+      id: "column-3",
+      title: "Review🪄",
+      items: [],
+    },
+    {
+      id: "column-4",
+      title: "Done✅",
+      items: [],
+    },
+  ],
+};
