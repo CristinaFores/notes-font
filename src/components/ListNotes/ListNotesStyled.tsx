acb1ed1c14d26fc4922655abd9ce49cc8c0d36e5
Cristina <cristinaforescampos1992@gmail.com>
2023-02-13 23:46:06 +0100

    Delete styles  ListNotes

--- src/components/ListNotes/ListNotesStyled.tsx
+++ src/components/ListNotes/ListNotesStyled.tsx
@@ -1,7 +1,3 @@
 import styled from "styled-components";
 
-export const ListNotesStyled = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-  gap: 1.2rem;
-`;
+export const ListNotesStyled = styled.ul``;
