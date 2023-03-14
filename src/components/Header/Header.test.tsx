18c22f477caab3fe1f46138b1601ffeb975eca25
Cristina <cristinaforescampos1992@gmail.com>
2023-03-14 17:52:04 +0100

    Fix test Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,5 +1,11 @@
 import { screen, render } from "@testing-library/react";
 import Header from "./Header";
+import * as router from "react-router";
+const navigate = jest.fn();
+
+beforeEach(() => {
+  jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+});
 
 describe("Given component Header", () => {
   describe("When it render", () => {
